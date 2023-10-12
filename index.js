7d7cfb9b79e843cdde73c49fa559fc6ee7cb13b9
RedSatura <111181391+RedSatura@users.noreply.github.com>
2023-10-12 19:54:03 +0800

    adding items complete

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+var items = 0
+
+var listTextData = {
+	
+}
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-	res.render("index.ejs");
+	listTextData = {};
+	res.render("index.ejs", {
+		listTextData: listTextData,
+	});
+});
+
+app.post("/new-item", (req, res) => {
+	items += 1;
+	listTextData["item" + String(items)] = "";
+	res.render("index.ejs", {
+		listTextData: listTextData,
+	});
 });
 
 app.listen(port);
+
+function underlineText() {
+	alert("hey");
+}
